7776ba4ec45b3339af9ad846932446a427cd461a
Jonathan.Francis <jjonathan.francis@gmail.com>
2023-08-08 23:28:12 -0400

    update app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://localhost:27017/fruitsDB") //name of database at the end.
+
+
+//1) Delete Operation - Deleting documents from the fruits collection of the fruitsDB Database.
+
+
+Fruit.deleteOne({name:"Apple"}, function(err){ // Method to delete a single document from the fruits Collection of the fruitsDB database.
+    if(err){
+      console.log(err);
+    }
+    else{
+      console.log("Sucessfully deleted the document.");
+    }
+  })
+
+
+  Fruit.deleteMany({name:"Orange"}, function(err){ // // Method to delete multiple documents from the fruits Collection of the fruitsDB database.
+    if(err){
+      console.log(err)
+    }
+    else{
+      console.log("Succesfully deleted all the document.") 
+    }
+  })
