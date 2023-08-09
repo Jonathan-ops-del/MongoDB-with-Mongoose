368f19f88bde4c71716455c1c265965d2915a7c5
Jonathan.Francis <jjonathan.francis@gmail.com>
2023-08-09 00:32:48 -0400

    update app.js files

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ const mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost:27017/fruitsDB") //name of database at the end.
 
 
+const fruitSchema = new mongoose.Schema({
+    name:{
+      type: String,
+      required:[true,"Please check your data entry, no name specified!"]
+    },
+    rating: {
+      type:Number,
+      min:1,
+      max:10
+    },
+    review:String
+  });
+  
+  const Fruit = mongoose.model("Fruit", fruitSchema) //collections called - fruits string - need to put singluar form.
+
+
 //1) Delete Operation - Deleting documents from the fruits collection of the fruitsDB Database.
 
 
