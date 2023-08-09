d90795851d370a325aff58d42021309f0af80d1e
Jonathan.Francis <jjonathan.francis@gmail.com>
2023-08-09 01:05:38 -0400

    udpdate connect

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set("strictQuery", true);
 
-mongoose.connect("mongodb://localhost:27017/fruitsDB") //name of database at the end.
+mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB",{useNewUrlParser: true}); //name of database at the end.
 
 
 const fruitSchema = new mongoose.Schema({
