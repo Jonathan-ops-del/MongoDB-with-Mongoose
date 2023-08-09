72a563ccd99032349a00e5755341dffb5b0b7588
Jonathan.Francis <jjonathan.francis@gmail.com>
2023-08-09 00:09:11 -0400

    udpated json file & app.js

--- app.js
+++ app.js
@@ -8,23 +8,23 @@ Fruit.find(function(err,fruits){
     if(err){
       console.lgo(err);
     } else{
-      console.log(fruits); //Displays all fruits from the fruits collections of the fruitsDB Database in the console.
+      console.log(fruits); //Displays all fruits from the fruits collection of the fruitsDB Database in the console.
   
       fruits.forEach(function(fruit){
-        console.log(fruit.name) // Display each individual fruits name from the fruits collections of the fruitsDB Database in the console.
+        console.log(fruit.name) // Display each individual fruits name from the fruits collection of the fruitsDB Database in the console.
       })
     }
   })
 
-  //// - 2) Read Operation - To view the people documents from the people collection from the fruitsDB database.
+  // - 2) Read Operation - To view the people documents from the people collection from the fruitsDB database.
   Person.find(function(err,people){
     if(err){
       console.log(err);
     } else{
-      console.log(people); //Displays all people from the people collections of the fruitsDB Database in the console.
+      console.log(people); //Displays all people from the people collection of the fruitsDB Database in the console.
   
-      fruits.forEach(function(people){
-        console.log(people.name) // Display each individuals name from the peoples collections of the fruitsDB Database in the console.
+      people.forEach(function(person){
+        console.log(person.name) // Display each individuals name from the people collection of the fruitsDB Database in the console.
       })
     }
   })
