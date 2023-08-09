e4511e4c476701d00626ca70ac12c8d8812a0ba1
Jonathan.Francis <jjonathan.francis@gmail.com>
2023-08-08 23:30:12 -0400

    updated app.js with double orange

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose'); //npm install mongoose
 mongoose.connect("mongodb://127.0.0.1:27017/fruitsDB",{useNewUrlParser: true}); //name of database at the end.
 
 
-// 1) - Create Operation : Inserting fruit documents into the Fruits Collection of the fruitsDB database.
+// 1) - Create Operation : Inserting fruit documents into the fruits collection of the fruitsDB database.
 
 const fruitSchema = new mongoose.Schema({
     name:{
@@ -41,6 +41,12 @@ const fruitSchema = new mongoose.Schema({
     rating:4,
     review: "Too sour for me."
   })
+
+  const oranges = new Fruit({
+    name:"Orange",
+    rating:4,
+    review: "Too sour for me."
+  })
   
   const banana = new Fruit({
     name:"Banana",
@@ -48,7 +54,7 @@ const fruitSchema = new mongoose.Schema({
     review:"Weird texture"
   })
   
-  Fruit.insertMany([kiwi,orange,banana], function(err){   // Method of inserting multiple documents into the fruits collection of the fruitsDB database.
+  Fruit.insertMany([kiwi,orange, oranges, banana], function(err){   // Method of inserting multiple documents into the fruits collection of the fruitsDB database.
     if (err){
       console.log(err);
     }
@@ -58,7 +64,7 @@ const fruitSchema = new mongoose.Schema({
   })
 
 
-// 2) Create Operation : Inserting people documents into the People Collection of the fruitsDB database.
+// 2) Create Operation : Inserting people documents into the people collection of the fruitsDB database.
 
 const personSchema = new mongoose.Schema({
     name: String,
